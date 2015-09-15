119506b6ab955a9da99d9ff198487c5d7ff03792
Shlomi <shlomisha@users.noreply.github.com>
2015-09-15 16:28:29 +0300

    Create TVNetIL_Favez0ne_Links_Fix.meta.js

---
+++ TVNetIL_Favez0ne_Links_Fix.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @id             TVNetIL_Favez0ne_Links_Fix
+// @name           TVNetIL Favez0ne Links Fix
+// @version        1.0.0
+// @namespace      shlomisha
+// @author         shlomisha
+// @description    Creates direct links to the favez0ne downloading pages from TVnetIL site
+// @include        http://www.tvnetil.net/review/*
+// @grant          none
+// @run-at         document-end
+// @updateURL      https://raw.githubusercontent.com/shlomisha/TVNetIL-Favez0ne-Links-Fix/master/TVNetIL_Favez0ne_Links_Fix.meta.js
+// @downloadURL    https://raw.githubusercontent.com/shlomisha/TVNetIL-Favez0ne-Links-Fix/master/TVNetIL_Favez0ne_Links_Fix.user.js
+// ==/UserScript==
